b0802a72c048f3a14c8e6ab14c006a6ed8f19e71
daniel-m10 <daniel.montecinos.pinto@gmail.com>
2025-09-13 22:26:40 -0400

    Add Category and Expense models with corresponding unit tests

---
+++ src/ExpenseTracker.Domain/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.Domain.Models
+{
+    public class Expense
+    {
+        public Guid Id { get; init; }
+        public string Description { get; init; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public Guid CategoryId { get; set; }
+        public DateTime CreatedAt { get; init; }
+    }
+}
